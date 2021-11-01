e05f075fa6eb0ea968c53c25e1c07f3375f6b3b4
Siyi Xie <54562760+isissi@users.noreply.github.com>
2021-11-01 10:08:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // Sum all the numbers in the arraylet 
+  let n = 0;
+  //check if is array
+  if(Array.isArray(array)){
+    for (let item of array) {
+        n += sumItems(item);
+      }
+  }else{
+    n = array;
+  }
+  return n;
 }
 
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+
+
 module.exports = sumItems;
+
